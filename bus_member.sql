ab35618b0df21370698388d8613d2f9616f427b1
chobolevel <104749958+chobolevel@users.noreply.github.com>
2022-05-23 15:47:39 +0900

    Create bus_member.sql
    홈페이지 회원정보를 등록하는 테이블 생성문

---
+++ bus_member.sql
@@ -0,0 +1,8 @@
+create table bus_member(
+id varchar2(50) primary key,
+password varchar2(50) not null,
+name varchar2(20) not null,
+gender varchar2(10) not null,
+address varchar2(60) not null,
+phone varchar2(20) not null,
+tel varchar2(20));
